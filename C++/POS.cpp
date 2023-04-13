65b0b8a9af6fb030551ca45d0e6c9597a41e0a89
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-04-13 15:37:27 +0900

    クラスとメンバ

--- C++/POS.cpp
+++ C++/POS.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "POS.h"
 
-//ŠÖ”’è‹`
 void POS::print()
 {
+	//‰æ–Êo—Íˆ—
 	printf("x:%1f,y:%1f\n", x, y);
 }
--- C++/main.cpp
+++ C++/main.cpp
@@ -3,18 +3,25 @@
 
 int main(void)
 {
-	POS pos;  //クラスPOSのオブジェクト
-	POS hutu;
+	//クラスPOSのオブジェクト作成
+	POS pos; 
 
-	//メンバに入れるデータ
+	//クラスPOSのオブジェクト作成
+	//複数オブジェクトを作っても作られるメモリが違うため、別のデータを持てる
+	POS hutu; 
+
+	//posメンバ変数に値を代入
 	pos.x = 100.0;
 	pos.y = 200.0;
 
+	//hutuメンバ変数に値を代入
 	hutu.x = 300.0;
 	hutu.y = 400.0;
 
-	//値を画面に表示
+	//posのメンバ関数を呼び出す
 	pos.print();
+
+	//hutuのメンバ関数を呼び出す
 	hutu.print();
 
 	return 0;
