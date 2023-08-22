226770b5ec212f4b4bb5daaa967ae1f0b19ed3b8
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-08-22 14:05:40 +0900

    使っていない変数の削除

--- ShootingGame/Bullet.h
+++ ShootingGame/Bullet.h
@@ -1,8 +1,9 @@
 #pragma once
+#include "Player.h"
+
 class Bullet
 {
 private:
-	int movebullet;
 	int g_bullet;
 	int g_menux;
 
--- ShootingGame/Player.cpp
+++ ShootingGame/Player.cpp
@@ -24,6 +24,10 @@ void Player::PlayerUpdate()
 	//左キーでカーソルを左に移動
 	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_LEFT) || KeyManager::OnKeyClicked(KEY_INPUT_LEFT)) {
 		--g_Player > 10;
+		if (PAD_INPUT::OnPressed(XINPUT_BUTTON_RIGHT_SHOULDER) || KeyManager::OnKeyPressed(KEY_INPUT_UP))
+		{
+			DrawCircle(101 + g_Player, 490, 5, 0xffff00, TRUE);
+		}
 	}
 	//右キーでカーソルを右に移動
 	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_RIGHT) || KeyManager::OnKeyClicked(KEY_INPUT_RIGHT)) {
