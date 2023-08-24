42711718fe6aa3713de70aa4401199f3b006a82c
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-08-24 14:57:32 +0900

    コメントアウト

--- ShootingGame/Bullet.cpp
+++ ShootingGame/Bullet.cpp
@@ -21,7 +21,7 @@ void Bullet::BUlletDraw()
 {
 	if (PAD_INPUT::OnPressed(XINPUT_BUTTON_RIGHT_SHOULDER) || KeyManager::OnKeyPressed(KEY_INPUT_DOWN))
 	{
-		WaitTime++;
+		//WaitTime++;
 		DrawCircle(101, 490, 5, 0xffff00, TRUE);
 	}
 
